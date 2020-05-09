249ce3562b15bdb0be3c65286e913345d2e8d769
dmathisen <dan@danmathisen.com>
2020-05-09 13:34:31 -0400

    set up Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import { BudgetPlanner } from './Components/BudgetPlanner/BudgetPlanner';
+import { ThankYou } from './Components/ThankYou/ThankYou';
 
 function App() {
   return (
-    <div className="App">
-      <BudgetPlanner />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Switch>
+          <Route exact path="/" component={BudgetPlanner} />
+          <Route path="/thank-you" component={ThankYou} />
+        </Switch>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
