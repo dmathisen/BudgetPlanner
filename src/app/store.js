27c0ebdc3efae2a4859816f0f9f1a9434cb2d99c
dmathisen <dan@danmathisen.com>
2020-05-09 11:50:02 -0400

    move login into Components directory

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from '../components/Login/loginSlice';
+import loginReducer from '../Components/Login/loginSlice'
 
 export default configureStore({
   reducer: {
