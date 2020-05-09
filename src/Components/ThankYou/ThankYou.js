908c390a8806ad8b410dab2b389f051ee103b398
dmathisen <dan@danmathisen.com>
2020-05-09 14:07:03 -0400

    pull global state into ThankYou

--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 export function ThankYou() {
   let history = useHistory();
 
+  // global state
+  const userName = useSelector(state => state.login.userName);
+  const contribution = useSelector(state => state.budgetPlanner.contribution);
+  const salary = useSelector(state => state.budgetPlanner.salary);
+
+  // events
   const handleGoBackClick = e => {
     e.preventDefault();
     history.push("/");
@@ -11,7 +18,36 @@ export function ThankYou() {
 
   return (
     <>
-      <h2>Thank you</h2>
+      <p className="lead">Thank you {userName}</p>
+
+      <div>
+        <label>
+          <span>Your Election</span>
+          <span>{contribution}%</span>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <span>Your Salary</span>
+          <span>${salary}</span>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <span>Your Expense</span>
+          <span>$x</span>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <span>Your Savings</span>
+          <span>$x</span>
+        </label>
+      </div>
+
       <a href="/" onClick={handleGoBackClick}>Go back</a>
     </>
   )
